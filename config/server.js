dce81d7a60f418dc1b238146f0b47451092ada2b
nyambayarsaint <nyambayar.saint@gmail.com>
2020-11-21 15:34:06 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0f81e5a87fc8708bb1397dc75b215a63'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2854bc54-001c-492a-a8d8-a820a166438f'
+};
